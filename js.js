f198028d1a74d9d9a55cedf644b744d690890dfe
Maciej Olszewski <hey@maciejolszewski.com>
2016-12-22 23:01:32 +0000

    init

--- js.js
+++ js.js
@@ -12,9 +12,9 @@ function numbers_picker() {
     }
   }
   rand.sort(sortit);
-  console.log(rand);
-  return(rand);
+  populate(rand);
 }
+
 
 function lookup(x) {
   return x == this;
@@ -25,22 +25,8 @@ function sortit(a, b){
 };
 
 
-// function numbers_picker() {
-//   var rand = [];
-//   var i;
-//   while (i<5){
-//     var x = Math.floor((Math.random()*59));
-//     if (rand.find(x){
-//       rand.push(x);
-//       i++;
-//     }
-//   }
-//   populate(rand);
-// }
-//
-// function populate(numbers) {
-//   console.log(numbers);
-//   for (var i=0; i<5; i++){
-//     container.appendChild(document.createTextNode(numbers[i]));
-//   }
-// }
+function populate(numbers) {
+  for (var i=0; i<5; i++){
+    container.appendChild(document.createTextNode(numbers[i]));
+  }
+}
